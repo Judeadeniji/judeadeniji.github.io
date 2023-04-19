6d882473b86c8727303c3279c25efefbeb68b5f4
Judeadeniji <judeadeniji66@gmail.com>
2023-04-19 14:47:38 +0100

    cc

---
+++ brace-docs-v2/assets/js/src_components_home_Content_js.chunk.js
@@ -0,0 +1,53 @@
+"use strict";
+(self["webpackChunkBraceJs"] = self["webpackChunkBraceJs"] || []).push([["src_components_home_Content_js"],{
+
+/***/ "./src/components/home/Content.js":
+/*!****************************************!*\
+  !*** ./src/components/home/Content.js ***!
+  \****************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var brace_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! brace-js */ "../brace-js/index.js");
+/* harmony import */ var brace_js__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(brace_js__WEBPACK_IMPORTED_MODULE_0__);
+
+const Content = () => {
+  return createElement("section", {
+    class: "content"
+  }, createElement("div", {
+    class: "sponsors"
+  }, createElement("h3", null, "Sponsors"), createElement("div", {
+    class: "sponsors-list"
+  }, createElement("div", null, createElement("div", {
+    class: "sponsor"
+  }), createElement("div", {
+    class: "sponsor"
+  }), createElement("div", {
+    class: "sponsor"
+  }), createElement("div", {
+    class: "sponsor"
+  }), createElement("div", {
+    class: "sponsor"
+  }), createElement("div", {
+    class: "sponsor"
+  }), createElement("div", {
+    class: "sponsor"
+  }), createElement("div", {
+    class: "sponsor"
+  }), createElement("div", {
+    class: "sponsor"
+  }), createElement("div", {
+    class: "sponsor"
+  })))), createElement("div", {
+    class: "features"
+  }, createElement("div", null, createElement("h4", null, "Performant"), createElement("p", null, "Optimized for speed and efficiency, ensuring that UI components render quickly and smoothly.")), createElement("div", null, createElement("h4", null, "Reliant"), createElement("p", null, "Works consistently across different browsers and devices, providing a reliable experience for users.")), createElement("div", null, createElement("h4", null, "Approachable"), createElement("p", null, "User-friendly API and clear documentation, making it easy for developers of all skill levels to use and customize."))));
+};
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ((0,brace_js__WEBPACK_IMPORTED_MODULE_0__.memo)(Content));
+
+/***/ })
+
+}]);
+//# sourceMappingURL=src_components_home_Content_js.chunk.js.map
